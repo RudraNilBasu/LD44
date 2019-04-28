15644ba88c2dc98ac68167ea1af66766d3320be0
Rudra Nil Basu <rudra.n.basu@gmail.com>
2019-04-28 17:06:43 +0530

    torch
    Signed-off-by: Rudra Nil Basu <rudra.n.basu@gmail.com>

---
+++ LD44/Assets/Scripts/Fire.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Fire : MonoBehaviour {
+
+    SpriteRenderer m_sprite_renderer;
+
+    [SerializeField]
+    Sprite[] m_sprites;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(PlayAnim());
+        m_sprite_renderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator PlayAnim()
+    {
+        print("Starting..");
+        int i = 0;
+        while(true) {
+            yield return new WaitForSeconds(0.5f);
+            i++;
+            print("i = " + i);
+            if (i >= 3) {
+                i = 0;
+            }
+            m_sprite_renderer.sprite = m_sprites[i];
+        }
+    }
+}
