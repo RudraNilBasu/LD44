b731a02b849c3dfa2d15d8656f25ebc0b23c249b
Rudra Nil Basu <rudra.n.basu@gmail.com>
2019-05-05 01:08:37 +0530

    level fixup and camera follow
    Signed-off-by: Rudra Nil Basu <rudra.n.basu@gmail.com>

---
+++ LD44/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField]
+    Transform target;
+
+    float multiplier = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(
+                Mathf.Lerp(transform.position.x, target.position.x, multiplier * Time.deltaTime),
+                Mathf.Lerp(transform.position.y, target.position.y, multiplier * Time.deltaTime), -10);
+	}
+}
